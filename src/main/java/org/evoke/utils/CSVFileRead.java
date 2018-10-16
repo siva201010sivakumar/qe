cb1ed8d529cdba0d33aeef66ca6f74eb666fc0dd
Siva Kumar Muppidi <smuppidi@evoketechnologies.com>
2018-10-16 14:50:51 +0530

    logs are added

--- src/main/java/org/evoke/utils/CSVFileRead.java
+++ src/main/java/org/evoke/utils/CSVFileRead.java
@@ -1,7 +1,6 @@
 package org.evoke.utils;
 
 import java.io.File;
-
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
@@ -9,17 +8,22 @@ public class CSVFileRead {
 
 	public String fileName;
 	private static final Logger log = LogManager.getLogger(CSVFileRead.class.getName());
-
 	public void readCSVToMap(String fileName) {
 		try {
 				this.fileName=fileName;
 				String filePath=FilePaths.getLocatorsPath()+File.separator+fileName;
 				if(!filePath.endsWith(".csv"))
 					filePath=filePath+".csv";
-				log.info("Reading the csv file [" +fileName+  " ] from the path [" +filePath+ "]");
-				File file=new File(filePath);	
+			log.info("Reading the csv file [" +fileName+  " ] from the path [" +filePath+ "]");
+				
+				File file=new File(filePath);
+				if(file.exists()) {
+					
+				}
+				else
+					throw new RuntimeException("File [" +filePath+ "] is NOT avilable to read.");
 		} catch (Exception e) {
-			
+			log.error(e.getMessage());
 		}
-	}
+	}	
 }
